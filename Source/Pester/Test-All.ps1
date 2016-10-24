75762c6c835ca85ea53e6206f27618f3937c2fbd
a.sarafian <dev1978@outlook.com>
2016-10-24 18:47:37 +0200

    no message

--- Source/Pester/Test-All.ps1
+++ Source/Pester/Test-All.ps1
@@ -2,18 +2,29 @@
 
 Param (
     [Parameter(Mandatory = $false)]
-    [string]$OutputPath=$null
+    [ValidateSet("None","Temp","ScriptRoot")]
+    [string]$OutputFolderPath="None"
 )
 $failedCount=0
 $pesterHash=@{
     Script="$PSScriptRoot"
     PassThru=$true
 }
+
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".").Replace(".ps1", "")
+switch ($OutputFolderPath)
+{
+    'None' {$outputPath=$null}
+    'ScriptRoot' {$outputPath="$PSScriptRoot\$sut.xml"}
+    'Temp' {$outputPath="$env:TEMP\$sut.xml"}
+}
+
 if($OutputPath)
 {
     $pesterHash.OutputFormat="NUnitXml"
-    $pesterHash.OutputPath=$OutputPath
+    $pesterHash.OutputPath=$outputPath
 }
+
 $pesterResult=Invoke-Pester @pesterHash
 
 if($pesterResult.FailedCount -gt 0)
