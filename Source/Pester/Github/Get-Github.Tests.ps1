36e389defb3cb35ae320cb175171c957a5529dba
Alex Sarafian <dev1978@outlook.com>
2016-10-25 08:36:00 +0200

    no message (#2)

--- Source/Pester/Github/Get-Github.Tests.ps1
+++ Source/Pester/Github/Get-Github.Tests.ps1
@@ -3,11 +3,16 @@ $scriptPath=Resolve-Path -Path "$PSScriptRoot\..\..\Scripts\Github\Get-Github.ps
 $repository="PowerShellScripts"
 $user="Sarafian"
 $developBranch="develop"
+$tag="v0.1"
+# File inside the zip is suffixed with the release name v0.1 vs 0.1
+$release="0.1"
 
 $expectedMasterZipPath=Join-Path $env:TEMP "$repository-master.zip"
 $expectedMasterPath=Join-Path $env:TEMP "$repository\$repository-master"
-$expectedDevelopZipPath=Join-Path $env:TEMP "$repository-develop.zip"
-$expectedDevelopPath=Join-Path $env:TEMP "$repository\$repository-develop"
+$expectedDevelopZipPath=Join-Path $env:TEMP "$repository-$developBranch.zip"
+$expectedDevelopPath=Join-Path $env:TEMP "$repository\$repository-$developBranch"
+$expectedTagZipPath=Join-Path $env:TEMP "$repository-$tag.zip"
+$expectedTagPath=Join-Path $env:TEMP "$repository\$repository-$release"
 
 Describe "Get-Github.ps1" {
     It "Download default master branch" {
@@ -25,19 +30,34 @@ Describe "Get-Github.ps1" {
         $expected=Get-Item -Path $expectedMasterPath
         $actual.FullName | Should BeExactly ($expected.FullName)
     }
-    It "Download specified develop branch" {
-        & $scriptPath -User $user -Repository $repository -Branch develop
+    It "Download specified $developBranch branch" {
+        & $scriptPath -User $user -Repository $repository -Branch $developBranch
         Test-Path -Path $expectedDevelopZipPath -PathType Leaf |Should BeExactly $true
-        $actual=& $scriptPath -User $user -Repository $repository -Branch develop
+        $actual=& $scriptPath -User $user -Repository $repository -Branch $developBranch
         $expected=Get-Item -Path $expectedDevelopZipPath
         $actual.FullName | Should BeExactly ($expected.FullName)
     }
-    It "Download and expand specified develop branch" {
-        #& $scriptPath -User $user -Repository $repository  -Branch develop -Expand
+    It "Download and expand specified $developBranch branch" {
+        #& $scriptPath -User $user -Repository $repository  -Branch $developBranch -Expand
         #Test-Path -Path $expectedDevelopZipPath -PathType Leaf |Should BeExactly $false
         #Test-Path -Path $expectedDevelopPath -PathType Container |Should BeExactly $true
-        $actual=& $scriptPath -User $user -Repository $repository  -Branch develop -Expand
+        $actual=& $scriptPath -User $user -Repository $repository  -Branch $developBranch -Expand
         $expected=Get-Item -Path $expectedDevelopPath
+        $actual.FullName | Should BeExactly ($expected.FullName)
+    }
+    It "Download specified $tag tag" {
+        & $scriptPath -User $user -Repository $repository -Tag $tag
+        Test-Path -Path $expectedTagZipPath -PathType Leaf |Should BeExactly $true
+        $actual=& $scriptPath -User $user -Repository $repository -Tag $tag
+        $expected=Get-Item -Path $expectedTagZipPath
+        $actual.FullName | Should BeExactly ($expected.FullName)
+    }
+    It "Download and expand specified $tag tag" {
+#        & $scriptPath -User $user -Repository $repository -Tag $tag -Expand
+#        Test-Path -Path $expectedTagZipPath -PathType Leaf |Should BeExactly $false
+#        Test-Path -Path $expectedTagPath -PathType Container |Should BeExactly $true
+        $actual=& $scriptPath -User $user -Repository $repository -Tag $tag -Expand
+        $expected=Get-Item -Path $expectedTagPath
         $actual.FullName | Should BeExactly ($expected.FullName)
     }
 <# Problem with pester
